a0e98ade4d8b915c97e02c57826f2de66eaff28a
sarah <jisnif2@gmail.com>
2024-06-11 13:00:16 -0500

    Initial Commit

---
+++ GroupSync/GroupSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupSyncApp.swift
+//  GroupSync
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GroupSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
